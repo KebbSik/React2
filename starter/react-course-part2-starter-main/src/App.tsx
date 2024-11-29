f7272badf13c09f66be5c63b80f6a45d2229bef5
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-29 20:44:32 +0100

    Exercise - Fetching Data

--- starter/react-course-part2-starter-main/src/App.tsx
+++ starter/react-course-part2-starter-main/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <div>
-      <TodoList />
+      {/* <TodoList /> */}
+      <PostList />
     </div>
   );
 }

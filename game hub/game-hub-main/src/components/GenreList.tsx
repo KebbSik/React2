cf21178440a716d6ed8b053788dd8544d30b9e7d
Kevin <kevinn.sarfoo1@gmail.com>
2024-12-07 07:23:25 +0100

    Exercise - Fetching Genres

--- game hub/game-hub-main/src/components/GenreList.tsx
+++ game hub/game-hub-main/src/components/GenreList.tsx
@@ -21,7 +21,12 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   if (error) return null;
 
-  if (isLoading) return <Spinner />;
+  if (isLoading)
+    return (
+      <>
+        <Spinner />
+      </>
+    );
 
   return (
     <>
@@ -29,7 +34,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image

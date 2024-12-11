6601f322bde88e059a4254ac331d8cb736d9837d
Kevin <kevinn.sarfoo1@gmail.com>
2024-12-11 06:19:19 +0100

    Exercise - Fetching Games

--- game hub/game-hub-main/src/hooks/usePlatforms.ts
+++ game hub/game-hub-main/src/hooks/usePlatforms.ts
@@ -2,7 +2,8 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
+
 
 interface Platform {
   id: number;
@@ -16,7 +17,8 @@ const usePlatforms = () => useQuery<Platform[],Error>({
     apiClient
     .get<FetchResponse<Platform>>('/platforms/lists/parents')
     .then(res => res.data),
-  staleTime: 24 * 60 * 60 * 1000
+  staleTime: 24 * 60 * 60 * 1000,
+  initialData: {count: platforms.length, results: platforms}
 });
 
 export default usePlatforms;

0e977a4ebffc27084e991d2625724201db228924
Kevin <kevinn.sarfoo1@gmail.com>
2024-12-13 07:03:21 +0100

    Exercise - symplifying time calculations

--- game hub/game-hub-main/src/hooks/useGenres.ts
+++ game hub/game-hub-main/src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>('/genres'); 
 
@@ -16,7 +16,7 @@ const useGenres = () =>
   useQuery<Genre[],Error>({
     queryKey: ['genres'],
     queryFn: () => apiClient.getAll, 
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
     initialData: {count: genres.length, results: genres }
   });
 

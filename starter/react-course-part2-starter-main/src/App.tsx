9f3972255717f04dacffadee8c79da86d1cf6faf
Kevin <kevinn.sarfoo1@gmail.com>
2024-12-18 06:38:22 +0100

    Consolidating State Logic with a Reducer

--- starter/react-course-part2-starter-main/src/App.tsx
+++ starter/react-course-part2-starter-main/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <div>
-      <TodoForm />
-      <TodoList />
+      <Counter />
+      {/* <TodoForm />
+      <TodoList /> */}
       {/* <PostList /> */}
     </div>
   );

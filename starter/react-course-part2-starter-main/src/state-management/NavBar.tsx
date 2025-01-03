bbf31545eed87274710671f1b2e7d4ca24c0042e
Kevin <kevinn.sarfoo1@gmail.com>
2025-01-03 20:25:37 +0100

    Preventing Unnecessary renders

--- starter/react-course-part2-starter-main/src/state-management/NavBar.tsx
+++ starter/react-course-part2-starter-main/src/state-management/NavBar.tsx
@@ -4,8 +4,9 @@ import TaskContext from "./tasks/tasksContext";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
+  console.log("Render NavBar");
   const { tasks } = useContext(TaskContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>

b6c38d5e43c7945fcbb0a37171e15ce15c6be536
Kevin <kevinn.sarfoo1@gmail.com>
2025-01-04 22:07:25 +0100

    Layout Routes

--- starter/react-course-part2-starter-main/src/routing/routes.tsx
+++ starter/react-course-part2-starter-main/src/routing/routes.tsx
@@ -7,6 +7,7 @@ import Layout from "./Layout";
 import UsersPage from "./UsersPage";
 import ErrorPage from "./ErrorPage";
 import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,11 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "login", element: <LoginPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
       {
         path: "users",
         element: <UsersPage />,

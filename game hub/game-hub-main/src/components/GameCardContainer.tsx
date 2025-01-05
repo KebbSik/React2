34da7f9c1dc0dcd04c98c8c5ab5725048a1f63b7
Kevin <kevinn.sarfoo1@gmail.com>
2025-01-05 11:04:00 +0100

    Added Hover effect to game card

--- game hub/game-hub-main/src/components/GameCardContainer.tsx
+++ game hub/game-hub-main/src/components/GameCardContainer.tsx
@@ -7,7 +7,12 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      _hover={{ transform: "scale(1.05)" }}
+      transition="transform .2s ease-in-out"
+      borderRadius={10}
+      overflow="hidden"
+    >
       {children}
     </Box>
   );

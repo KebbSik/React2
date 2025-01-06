abdb1eb4e5bbf6f4a2f9857085eb8d9f4eb05fbc
Kevin <kevinn.sarfoo1@gmail.com>
2025-01-06 14:08:45 +0100

    Build Expandable text

--- game hub/game-hub-main/src/pages/GameDetailPage.tsx
+++ game hub/game-hub-main/src/pages/GameDetailPage.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
+  const [shownLines, setShownLines] = useState(2);
+  const [btnText, setBtnText] = useState("Show more");
+
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
@@ -14,7 +18,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
